5bb925a04b437b7db0da12b99afe2fae1eec622f
Vilmar Cabañero <vilmarcabanero@gmail.com>
2025-02-18 22:22:24 +0800

    Update package.json to move @capacitor/android and @capacitor/cli to dev dependencies and set up manual chunking of dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split large dependencies into separate chunks
+          react: ['react', 'react-dom'], // Example splitting React into its own chunk
+          capacitor: ['@capacitor/core'],
+          // Add more libraries as needed
+        },
+      },
+    },
+  }
 })
