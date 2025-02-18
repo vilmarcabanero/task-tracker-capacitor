911c241c77915b46fa360e2b1bc781480286deac
Vilmar Cabañero <vilmarcabanero@gmail.com>
2025-02-18 20:38:37 +0800

    Setup capacitor for native android.

---
+++ android/app/src/main/java/com/vilmarcabanero/tasktracker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vilmarcabanero.tasktracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vilmarcabanero.tasktracker',
+  appName: 'Task Tracker',
+  webDir: 'dist'
+};
+
+export default config;
